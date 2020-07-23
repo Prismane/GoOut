b91266b74f0e43156a74a0585f84ecd4954752a7
Hendricks-3D <odainbrown111@gmail.com>
2020-07-23 15:53:23 -0500

    map implemented

--- src/app/pages/feeds/feeds.page.ts
+++ src/app/pages/feeds/feeds.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
+declare var google;
 @Component({
   selector: 'app-feeds',
   templateUrl: './feeds.page.html',
@@ -7,17 +8,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedsPage implements OnInit {
 
+  @ViewChild("map",{ static: false }) mapElement;
+
+
+  map: any;
+
+  latitude = 18.1096;
+  longitude=-77.2975;
 
   private segmentModel = 'featured';
   constructor() { }
 
   ngOnInit() {
+
+    
   }
 
+  ngAfterContentInit():void{
+
+    (async () => { 
+
+
+      await this.delay(5000);
+
+      this.initMap();
+
+  })();
+
+
+  }
 
   public segmentChanged(event)
   {
 
   }
+
+
+
+  public  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+    /**
+   * Method that initialize map and display it in the div tag by @ViewChild 
+   */
+  initMap() {
+    // new google.maps.LatLng(this.latitude,this.longitude);
+ 
+   let latlng =new google.maps.LatLng(this.latitude,this.longitude);
+     //MAP OPTIONS
+     let mapOptions={
+       center:latlng,
+       zoom:15,
+       mapTypeId: google.maps.MapTypeId.ROADMAP,
+       disableDefaultUI: true
+     };
+ 
+  
+     this.map = new google.maps.Map(this.mapElement.nativeElement,mapOptions);//CREATE MAP WITH PRESET OPTIONS
+ 
+       //CREATE MAP MARKER
+       let marker = new google.maps.Marker({
+         position: latlng,
+         map: this.map,
+         label:"You"
+       }); 
+ 
+   
+ 
+  
+   }//END OIF INTIALIZE MAP METHOD
 
 }
